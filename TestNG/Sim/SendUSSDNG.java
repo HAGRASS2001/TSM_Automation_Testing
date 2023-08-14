b7cba93966603ea316a8ded5d717ac5c66e5279b
HAGRASS2001 <83500189+HAGRASS2001@users.noreply.github.com>
2023-08-14 16:05:34 +0300

    edit

--- TestNG/Sim/SendUSSDNG.java
+++ TestNG/Sim/SendUSSDNG.java
@@ -54,7 +54,7 @@ public class SendUSSDNG {
 		proxy.setSocksProxy("127.0.0.1:8080");
 		proxy.setSocksVersion(5);
 		options.setProxy(proxy);
-		//options.setHeadless(true);
+		options.setHeadless(true);
 		driver = new FirefoxDriver(options);
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
